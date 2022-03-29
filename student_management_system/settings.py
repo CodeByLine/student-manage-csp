2d4e4ee4d7fba464ac1da5bc4ccb03cbb04027c2
Y Leventhal <ysl@MBP21.local>
2022-03-29 15:06:33 -0400

    config

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+# SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
