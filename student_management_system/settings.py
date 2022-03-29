154daaa3a28c686860f1faf4362327dd477f0230
Y Leventhal <ysl@MBP21.local>
2022-03-29 13:52:48 -0400

    pre

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,7 +154,7 @@ try:
 except ImportError:
     print("Looks like no local file. You must be on production")
 
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import dj_database_url
 prod_db=dj_database_url.config(conn_max_age=500, ssl_require=True)
